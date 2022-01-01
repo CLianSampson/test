e380e5c9591623361386ca5de4de55deb345efea
Sampson Chen <448169133@qq.com>
2022-01-01 20:12:08 +0800

    Update Student.java

--- src/main/java/com/cl/test/model/Student.java
+++ src/main/java/com/cl/test/model/Student.java
@@ -18,5 +18,7 @@ public class Student {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+System.out.println("hello world");
+System.out.println("hello world");
     }
 }
