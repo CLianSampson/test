d6f766a171f442e4a7dd3162cc6546ea5d05a8cb
Sampson Chen <448169133@qq.com>
2022-01-01 20:09:20 +0800

    加个测试

--- src/main/java/com/cl/test/model/Student.java
+++ src/main/java/com/cl/test/model/Student.java
@@ -15,4 +15,8 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
