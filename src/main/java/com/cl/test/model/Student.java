dd0a47e0365b9d7e9f63f9def169c615ffbc244e
Sampson Chen <448169133@qq.com>
2022-01-01 20:12:48 +0800

    加个测试merge

--- src/main/java/com/cl/test/model/Student.java
+++ src/main/java/com/cl/test/model/Student.java
@@ -18,5 +18,7 @@ public class Student {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
     }
 }
